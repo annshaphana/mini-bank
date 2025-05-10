0ba21891522ef55d8ff31b10c3e7a5abd886b14e
annshaphana <sujensujenthini@gmail.com>
2025-05-10 13:04:09 +0530

    your msg

--- MiniBank.py
+++ MiniBank.py
@@ -1,5 +1,5 @@
 accounts = {}
-account_number_counter = 1000  
+account_number_counter = {}
 users = {"admin": "password123"}  
 
 def create_account():
@@ -75,74 +75,6 @@ def check_balance():
     else:
         print("Account not found")
 
-
-while True:
-    print("\nBanking System Menu:")
-    print("1. Create Account")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. View Transactions")
-    print("5.check balance")
-    print("6. User Login")
-    print("7. Exit")
-
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-        create_account()
-    elif choice == "2":
-        deposit()
-    elif choice == "3":
-        withdraw()
-    elif choice == "4":
-        view_transactions()
-    elif choice == "5":
-        check_balance()
-    elif choice == "6":
-        user_login()
-        break
-    elif choice == "7":
-         print("Exiting system... Thank you!")
-    else:
-        print("Invalid choice! Please try again.")
- 
-
-def Admin():
-    admin_name= input("Enter name:")
-    admin_password= input("Enter password:")
-
-    with open("admin.txt",'w')as file:
-        file.write(f"{admin_name}/t{admin_password}/n")
-
-    if admin_name and admin_password:
-        print("Login successfully")
-    else:
-        print("Login failed")
-
-Admin()
-import os
-def customer_info():
-    name=input("Enter name:")
-    address=input("Enter address:")
-    username=input("Enter user_name:")
-    userpassword=input("Enter user_password:")
-    
-
-
-
-def create_next_customer_id():
-    if not os.path.exists("customers.txt"):
-        return 'c001'
-    else:
-        with open("customer.txt",'r')as file:
-            return f'c(int(file.readline()))'
-
-def create_customer():
-    customer=customer_info()
-    create_next_customers_id= create_next_customers_id()
-    with open ("customers.txt",'a')as customers_file,open("users.txt",'a')as users_file:
-        customers_file.write(f'{customer[0]}/t{customer[1]}\n')
-        users_file.write(f'{customer[2]}/t{customer[3]}/t')
 import os
 
 
@@ -167,6 +99,7 @@ def create_next_customer_id():
             last_id = int(last_line[1:])  
             return f'c{last_id + 1}' 
 
+
 def create_customer():
     customer = customer_info() 
     customer_id = create_next_customer_id()  
@@ -178,12 +111,6 @@ def create_customer():
     print(f"Customer {customer_id} added successfully!")
 
 
-create_customer()
-
-
-
-
-   
 
 def save_transaction():
     acc_no=input("Enter account number:")
@@ -191,7 +118,114 @@ def save_transaction():
     with open("transactions.txt", "a") as file:  
         file.write(f"{acc_no}/t{save_transaction}\n")
 
-save_transaction()
+
+
+
+
+while True:
+    print("\n****** Banking System Menu ******")
+    print("1. Create Account")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. View Transactions")
+    print("5.check balance")
+    print("6. User Login")
+    print("7. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        create_account()
+        print("**********")
+    elif choice == "2":
+        deposit()
+        print("**********")
+    elif choice == "3":
+        withdraw()
+        print("**********")
+    elif choice == "4":
+        view_transactions()
+        print("**********")
+    elif choice == "5":
+        check_balance()
+        print("**********")
+    elif choice == "6":
+        user_login()
+        print("**********")
+        break
+    elif choice == "7":
+         print("Exiting system... Thank you!")
+         print("**********")
+    else:
+        print("Invalid choice! Please try again.")
+ 
+
+def Admin():
+    admin_name= input("Enter name:")
+    admin_password= input("Enter password:")
+
+    with open("admin.txt",'w')as file:
+        file.write(f"{admin_name}/t{admin_password}/n")
+
+    if admin_name and admin_password:
+        print("Login successfully")
+    else:
+        print("Login failed")
+
+Admin()
+
+
+
+
+
+
+
+
+# import os
+
+
+# def customer_info():
+#     name = input("Enter name: ")
+#     address = input("Enter address: ")
+#     username = input("Enter username: ")
+#     userpassword = input("Enter user password: ")
+    
+#     return name, address, username, userpassword  
+
+
+# def create_next_customer_id():
+#     if not os.path.exists("customers.txt"):  
+#         return "c001"
+#     else:
+#         with open("customers.txt", 'r') as file:
+#             lines = file.readlines() 
+#             if not lines:  
+#                 return "c001"
+#             last_line = lines[-1].split("\t")[0] 
+#             last_id = int(last_line[1:])  
+#             return f'c{last_id + 1}' 
+
+
+# def create_customer():
+#     customer = customer_info() 
+#     customer_id = create_next_customer_id()  
+    
+#     with open("customers.txt", 'a') as customers_file, open("users.txt", 'a') as users_file:
+#         customers_file.write(f'{customer_id}\t{customer[0]}\t{customer[1]}\n')  
+#         users_file.write(f'{customer[2]}\t{customer[3]}\n')  
+        
+#     print(f"Customer {customer_id} added successfully!")
+
+
+# create_customer()
+
+
+
+
+   
+
+
+# save_transaction()
 
 
                     
